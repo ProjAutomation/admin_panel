f499050fab56c411c4c7d3148b8e9c67968fc2ce
Виктория Федорова <fiodorova.vika301296@yandex.ru>
2023-10-18 10:26:17 +0300

    Create models

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,47 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CustomUser(AbstractUser):
-    ...
+    BEGINNER = 'Новичок'
+    BEGINNER_PLUS = 'Новичок+'
+    JUNIOR = 'Джун'
+    SKILL_CHOICES = [
+        (BEGINNER,),
+        (BEGINNER_PLUS,),
+        (JUNIOR,),
+    ]
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Имя')
+    surname = models.CharField(
+        max_length=200,
+        verbose_name='Фамилия')
+    email = models.EmailField(
+        max_length=254,
+        verbose_name='Электронный адрес',
+        unique=True)
+    skill = models.CharField(
+        max_length=9,
+        choices=SKILL_CHOICES,
+        default=BEGINNER)
+    from_far_east = models.BooleanField(
+        default=False,
+        verbose_name='С Дальнего Востока')
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+
+    @property
+    def is_from_far_east(self):
+        return self.from_far_east
+
+    @is_from_far_east.setter
+    def is_from_far_east(self, value):
+
+        if self.is_staff:
+            self.from_far_east = value
+        else:
+            raise PermissionError(
+                'Only admin users can set "from_far_east" attribute.'
+                )
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,58 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from accounts.models import CustomUser
+
+
+class ProjectManager(AbstractUser):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Имя')
+    surname = models.CharField(
+        max_length=200,
+        verbose_name='Фамилия')
+    email = models.EmailField(
+        max_length=254,
+        verbose_name='Электронный адрес',
+        unique=True)
+    telegram = models.CharField(
+        max_length=200,
+        verbose_name='Телеграмм')
+    start_from = models.TimeField(
+        verbose_name='Начало рабочего дня')
+    end_from = models.TimeField(
+        verbose_name='Конец рабочего дня'
+    )
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+
+
+class TimeSlot(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    is_occupied = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.start_time} - {self.end_time}'
+
+    def is_available(self):
+        return not self.is_occupied
+
+
+class Team(models.Model):
+    member = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='team',
+    )
+    time_slot = models.OneToOneField(
+        TimeSlot,
+        on_delete=models.CASCADE,
+        related_name='time_slot',
+    )
+    project_manager = models.ForeignKey(
+        ProjectManager,
+        on_delete=models.CASCADE,
+        related_name='project_manager'
+    )
